3170fe4182cc183b51c09ab71573af0d039bc44f
Matt Joyce <matt.joyce@gmail.com>
2013-05-28 21:02:42 +1000

    added main, corrected various PEP violations

--- Generate.py
+++ Generate.py
@@ -1,29 +1,43 @@
-from PIL import Image, ImageDraw, ImageFont, ImageColor 
+"""Create a set of images of rendered text"""
+import sys
 import string
+from PIL import Image, ImageDraw, ImageFont 
 from  random import random
 import colorsys
 
-def Generate(W,H,msg,img_fraction,fontname,color):
-	msg=msg.replace("\n","")
-	im =Image.new("RGB",(W,H),(0,0,0))
-	draw = ImageDraw.Draw(im)
+def generate(im_width, im_height, text, img_scale, fontname):
+    """Create a new jpg file.
+    Render the text, in the middle
+    Adjust the font size to utilize the space
+    """
+        
+    text = str(text).replace("\n", "").strip()
+    new_image = Image.new("RGB", (im_width, im_height), (0, 0, 0))
+    draw = ImageDraw.Draw(new_image)
 
-	fontsize=1
-	font = ImageFont.truetype(fontname,fontsize)
-	while font.getsize(msg)[1] < img_fraction*im.size[1] and font.getsize(msg)[0] < img_fraction*im.size[0]  :
-		fontsize += 1
-		font = ImageFont.truetype(fontname, fontsize)
-	w, h = font.getsize(msg)
-	color=colorsys.hsv_to_rgb(random(),0.5, 0.95)
-	color_rgb=tuple([int(256*x) for x in color])
-	print color
-	print color_rgb
-	draw.text(((W-w)/2,(H-h)/2), msg, fill=(color_rgb), font=font)
-	fsp=msg.translate(string.maketrans("",""), string.punctuation)
-	im.save(fsp+".jpg", "jpeg")
+    fontsize = 1
+    font = ImageFont.truetype(fontname, fontsize)
+    text_width, text_height = font.getsize(text)
+    
+    while (text_width < img_scale*im_width and 
+            text_height < img_scale * im_height)  :
+        fontsize += 1
+        font = ImageFont.truetype(fontname, fontsize)
+        text_width, text_height = font.getsize(text)
+        
+    font = ImageFont.truetype(fontname, fontsize-1)
+    color = colorsys.hsv_to_rgb(random(), 0.5, 0.95)
+    color_rgb = tuple([int(256*x) for x in color])
+    draw.text(((im_width-text_width) / 2, (im_height-text_height) / 2), 
+                text, fill = (color_rgb), font=font)
+    filename = text.translate(string.maketrans("",""), string.punctuation)
+    new_image.save(filename+".jpg", "jpeg")
 
-	
-	
-lines=open("FrysPhrases.txt").readlines()
-for line in lines:
-	Generate(480,234,line,0.9,"comic.ttf",'white')
+def main(argv=None):
+    """ Read a text file, take each line and create a image file"""
+    lines = open("test.txt").readlines()
+    for line in lines:
+        generate(480, 234, line, 1, "comic.ttf")
+
+if __name__ == "__main__":
+    sys.exit(main())
